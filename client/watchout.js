629ba5c352d2b430c2218b77d4ff67deb5f72d82
Chris Tsang <chris.p.tsang@gmail.com>
2015-07-10 22:20:30 -0700

    Corrected selection and manipulation of enemies
    Added transitions for movement of enemies

--- client/watchout.js
+++ client/watchout.js
@@ -10,20 +10,20 @@ var gameOptions = {
 var gameStats = {
   score: 0,
   bestScore: 0
-}
+};
 
 var axes = {
-  // x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
-  // y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
+  x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
+  y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
 };
 
 var container = d3.select('.container');
 container.style('margin', '50px');
  
-var gameBoard = d3.select('.container').append('svg:svg')
+var gameBoard = d3.select('.container').append('svg')
   .attr('width', gameOptions.width)
   .attr('height', gameOptions.height)
-  .style('background-color', 'black');
+  .style('background-color', 'white');
 
 
 var updateScore = function() {
@@ -38,9 +38,6 @@ var updateBestScore = function () {
     .text(gameStats.bestScore.toString());
 };
 
-
-
-
 //create enemies
 var createEnemies = function() {
 
@@ -49,64 +46,43 @@ var createEnemies = function() {
   return _.map(enemies, function(enemy, index) {
     return {
       id: index,
-      x: Math.random() * gameOptions.width,
-      y: Math.random() * gameOptions.height
+      x: Math.random() * 100,
+      y: Math.random() * 100
     };
   });
-  
 };
 
 var render = function(enemy_data) {
   
   // console.log(enemy_data.length);
+  var enemies = gameBoard.selectAll('circle.enemy').data(enemy_data);
 
-  var enemies = gameBoard.selectAll('circle.enemy');
-  // we don't understand this::::
-    // .data(enemy_data, function(d) { 
-    //   // console.log(d);
-    //   return d.id;
-    // });
-
-  // console.log(enemies);
-
+  enemies.transition()
+    .duration(1500)
+    .attr('cx', function(enemy) {return axes.x(enemy.x);})
+    .attr('cy', function(enemy) {return axes.y(enemy.y);});
+    
+    
   enemies.enter()
     .append('svg:circle')
     .attr('class', 'enemy')
-    .transition().duration(500).attr('r', 50)
-    .attr('cx', function(enemy) {
-      return enemy.x;
-    })
-    .attr('cy', function(enemy) {
-      // console.log(enemy);
-      return enemy.y;
-    })
-    .attr('r', 20)
-    .attr('fill', 'red');
-    // .transition()
-    // .duration(1000)
-    // .attr('fill', 'green');
-
-  // enemies.exit()
-  //   .remove();
-
-  // return enemies.transition().duration(500).tweenstyle();
+    .attr('cx', gameOptions.width/2)
+    .attr('cy', gameOptions.height/2)
+    .transition()
+    .duration(500)
+    .attr('cx', function(enemy) {return axes.x(enemy.x);})
+    .attr('cy', function(enemy) {return axes.y(enemy.y);})
+    .attr('r', 10)
+    .attr('fill', 'black');
 };
-
 
 var play = function() {
 
-  var gameTurn = function() {
-    var newEnemyPositions = createEnemies();
-    render(newEnemyPositions);
-  };
 
-
-  gameTurn();
+  render(createEnemies());
   // console.log('-------');
-  setTimeout(gameTurn, 2000);        
+  setInterval(function(){return render(createEnemies());}, 2000);
   // setInterval(gameTurn, 2000);        
-
-
 };
 
 play();
